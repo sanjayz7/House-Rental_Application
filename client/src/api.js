5a439d788ed16d359272458d1dc0ab87a69f9c61
Sanjay <sanjayk91940@gmail.com>
2025-11-20 19:41:30 +0530

    Updated

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: "https://house-rental-application-1.onrender.com/api"
 });
+
 
 
 export const setAuthToken = (token) => {
